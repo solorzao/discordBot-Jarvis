b224279fbc00cc7cc437f92fbe921e1bf94c0833
solorzao <solorzao@oregonstate.edu>
2020-12-25 13:07:44 -0800

    added message functionality for main chat w/ exception handling, gave Jarvis roasting capabilities

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from discord.ext import commands
@@ -34,6 +35,35 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    roasts = [
+        f'A little known fact is that a long time ago @{message.author.name} used to work at McDonald’s. It was the last time anyone said about your work, “I’m lovin’ it.”',
+        f'We are doing this roast tonight to help @{message.author.name} live out one of his sexual fantasies, to have a room full of his friends shit all over him.',
+        f'It’s nice to see such a diverse crowd here today. We’ve got Indians, Jews, Whites, and whatever the fuck @{message.author.name} is.', 
+        f'@{message.author.name} you’re looking pretty rough this evening. @{message.author.name} looks like if sweatpants were a person.', 
+        f'@{message.author.name} if laughter is the best medicine, your face must be curing the world.'
+    ]
+
+    if message.content == 'roast me!':
+        response = random.choice(roasts)
+        await message.channel.send(response)
+
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 
 
 client.run(TOKEN)
